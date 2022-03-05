6ff908a40b4fdfd48e4c4cbd70070ddc320a22cd
Zakir Baytar <zakirrbaytar@gmail.com>
2022-03-05 11:43:13 +0300

    Create initial folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import Routes from './routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <header className="App-header">Header</header>
+      <Routes />
+      <footer>Footer</footer>
+    </Fragment>
   );
 }
 
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,6 @@
+import React, { FunctionComponent } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const AppRoutes: FunctionComponent = () => <Routes>{/** TODO: Add routes here */}</Routes>;
+
+export default AppRoutes;
