4a3954d7ca1e827a1527388f9eced28e911bfae9
Zakir Baytar <zakirrbaytar@gmail.com>
2022-03-08 10:23:36 +0300

    Add action types for game

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -1,6 +1,8 @@
-export enum ActionTypes {
+enum ActionTypes {
   AddWord = 'ADD_WORD',
   SetTurn = 'SET_TURN',
   StartNewGame = 'START_NEW_GAME',
   SetWinner = 'SET_WINNER',
 }
+
+export default ActionTypes;
