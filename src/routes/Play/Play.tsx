274868176f0c3c4770cb55a31419c45652b1aecb
Zakir Baytar <zakirrbaytar@gmail.com>
2022-03-08 15:02:35 +0300

    Add Play route page

---
+++ src/routes/Play/Play.tsx
@@ -0,0 +1,53 @@
+import React, { FunctionComponent } from 'react';
+import useGameManager from '../../hooks/useGameManager';
+import { GameState, Turn } from '../../types/game';
+
+import Player from '../../containers/Player';
+import Timer from '../../components/Timer';
+import GameOverModal from './GameOverModal';
+import PlayerSelectionControls from './PlayerSelectionControls';
+
+import './style.css';
+
+const Play: FunctionComponent = () => {
+  const { startNewGame, gameState, timeLeft } = useGameManager();
+
+  return (
+    <main className="container play-container is-max-widescreen">
+      <GameOverModal />
+      <section className="section">
+        <div className="columns is-mobile">
+          <div className="column is-5-desktop is-5-tablet is-4-mobile">
+            <Player name="Bilgisayar" imageSrc="./ai.svg" position="left" turn={Turn.Computer} />
+          </div>
+          <div className="column is-4-mobile is-flex is-justify-content-center is-align-content-center">
+            {gameState === GameState.Started ? (
+              <Timer seconds={8} timeLeft={timeLeft} />
+            ) : (
+              <div className="buttons are-small">
+                <button
+                  type="button"
+                  className="button is-small is-responsive is-primary is-align-self-center"
+                  onClick={() => startNewGame()}
+                >
+                  Yeni Oyuna Başla
+                </button>
+              </div>
+            )}
+          </div>
+          <div className="column is-5-desktop is-5-tablet is-4-mobile">
+            <Player name="Siz" imageSrc="./player.svg" position="right" turn={Turn.Player} />
+          </div>
+        </div>
+      </section>
+      {gameState !== GameState.Started && (
+        <section className="is-flex is-flex-direction-column is-align-items-center">
+          <p className="is-size-7 mb-2">Oyuna kimin başlayacağını seç</p>
+          <PlayerSelectionControls />
+        </section>
+      )}
+    </main>
+  );
+};
+
+export default Play;
