a123147834f71e749ef6c1d8be11275305cb0f8d
Zakir Baytar <zakirrbaytar@gmail.com>
2022-03-08 14:54:40 +0300

    Add modal for game over screen

---
+++ src/routes/Play/GameOverModal/GameOverModal.tsx
@@ -0,0 +1,86 @@
+import React, { Fragment, FunctionComponent, useEffect, useMemo, useState } from 'react';
+import cs from 'classnames';
+import useGameManager from '../../../hooks/useGameManager';
+
+import Modal from '../../../components/Modal';
+import capitalize from '../../../utils/capitalize';
+
+import { Turn } from '../../../types/game';
+
+import './style.css';
+
+const GameOverModal: FunctionComponent = () => {
+  const { winner, wordHistory } = useGameManager();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (winner) {
+      setIsOpen(true);
+    }
+  }, [winner]);
+
+  const { title, subtitle } = useMemo(() => {
+    if (winner === Turn.Computer) {
+      return {
+        title: 'Bilgisayar Kazandı!',
+        subtitle: 'Senin daha başarılı olabileceğine inanıyorum',
+      };
+    }
+
+    return {
+      title: 'Tebrikler!',
+      subtitle: 'Bilgisayara karşı başarılı olacağını biliyordum',
+    };
+  }, [winner]);
+
+  return (
+    <Modal
+      className={cs('game-over-modal', {
+        'game-won': winner === Turn.Player,
+        'game-lost': winner === Turn.Computer,
+      })}
+      isOpen={isOpen}
+      toggle={() => setIsOpen(false)}
+    >
+      <div className="modal-content">
+        <article className="message">
+          <div className="message-header is-flex-direction-column">
+            <h1 className="is-size-4 has-text-centered has-text-weight-bold mb-2">{title}</h1>
+            <h2 className="is-size-7 has-text-centered has-text-weight-semibold">{subtitle}</h2>
+            <button className="delete" onClick={() => setIsOpen(false)} />
+          </div>
+          <div className="message-body">
+            {wordHistory.length ? (
+              <Fragment>
+                <h1 className="is-size-6 is-underlined mb-2 has-text-centered">
+                  Söylenen kelimeler
+                </h1>
+                <ul>
+                  {wordHistory.map((word: string) => (
+                    <li key={word}>
+                      <p className="is-size-7 has-text-centered">{capitalize(word)}</p>
+                    </li>
+                  ))}
+                </ul>
+              </Fragment>
+            ) : (
+              <p className="is-size-7 has-text-centered mt-4">
+                Maalesef sözlükteki kelimelerden herhangi biri söylenmeden oyun bitti
+              </p>
+            )}
+          </div>
+          <div className="message-footer is-clearfix">
+            <button
+              className="button is-small is-outlined is-pulled-right"
+              onClick={() => setIsOpen(false)}
+            >
+              Tamam
+            </button>
+          </div>
+        </article>
+      </div>
+    </Modal>
+  );
+};
+
+export default GameOverModal;
