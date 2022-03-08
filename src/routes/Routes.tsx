2fbda496840fce9fe173df742498ccbb58cc4830
Zakir Baytar <zakirrbaytar@gmail.com>
2022-03-08 15:06:03 +0300

    Add routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,34 @@
 import React, { FunctionComponent } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import GameManagerProvider from '../context/GameManagerProvider';
 
-const AppRoutes: FunctionComponent = () => <Routes>{/** TODO: Add routes here */}</Routes>;
+import Home from './Home';
+import HowToPlay from './HowToPlay';
+import Play from './Play';
+
+import names from '../names.json';
+
+const AppRoutes: FunctionComponent = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/how-to-play" element={<HowToPlay />} />
+    <Route
+      path="/play"
+      element={
+        <GameManagerProvider
+          timer={8}
+          aiOptions={{
+            lossPercentage: 20,
+            timeToAnswer: 3,
+          }}
+          language="tr-TR"
+          words={names}
+        >
+          <Play />
+        </GameManagerProvider>
+      }
+    />
+  </Routes>
+);
 
 export default AppRoutes;
