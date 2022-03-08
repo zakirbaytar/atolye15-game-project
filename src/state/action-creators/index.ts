544d46603ca0c92ab8c7deef874c6ab865050213
Zakir Baytar <zakirrbaytar@gmail.com>
2022-03-08 10:38:40 +0300

    Add action creators for game actions

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,36 @@
+import { Dispatch } from 'react';
+import { Turn } from '../../types/game';
+import ActionTypes from '../action-types';
+import Action from '../actions';
+
+const addWord = (dispatch: Dispatch<Action>) => (word: string) => {
+  dispatch({ type: ActionTypes.AddWord, payload: { word } });
+};
+
+const setTurn = (dispatch: Dispatch<Action>) => (turn: Turn) => {
+  dispatch({ type: ActionTypes.SetTurn, payload: { turn } });
+};
+
+const startNewGame = (dispatch: Dispatch<Action>) => (turn?: Turn) => {
+  dispatch({ type: ActionTypes.StartNewGame, payload: { startingPlayer: turn } });
+};
+
+const setWinner = (dispatch: Dispatch<Action>) => (turn: Turn) => {
+  dispatch({ type: ActionTypes.SetWinner, payload: { turn } });
+};
+
+interface BoundActionCreators {
+  addWord: (word: string) => void;
+  setTurn: (turn: Turn) => void;
+  startNewGame: (turn?: Turn) => void;
+  setWinner: (turn: Turn) => void;
+}
+
+const bindActionCreators = (dispatch: Dispatch<Action>): BoundActionCreators => ({
+  addWord: addWord(dispatch),
+  setTurn: setTurn(dispatch),
+  startNewGame: startNewGame(dispatch),
+  setWinner: setWinner(dispatch),
+});
+
+export default bindActionCreators;
